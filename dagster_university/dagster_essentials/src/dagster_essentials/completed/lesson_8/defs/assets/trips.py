7f62420d3afc6b3eb293b8e3f20305f38897acb6
Yuri Idalgo de Matos da Silva <x432601@rede.sp>
2026-07-02 09:52:29 -0300

    Add. novo progresso no curso da Dagster

--- dagster_university/dagster_essentials/src/dagster_essentials/completed/lesson_8/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/completed/lesson_8/defs/assets/trips.py
@@ -3,8 +3,9 @@ import pandas as pd
 import requests
 from dagster_duckdb import DuckDBResource
 
+# src/dagster_essentials/defs/assets/trips.py
 from dagster_essentials.completed.lesson_8.defs.assets import constants
-from dagster_essentials.completed.lesson_8.defs.partitions import monthly_partition
+from dagster_essentials.defs.partitions import monthly_partition
 
 
 @dg.asset(
@@ -82,34 +83,33 @@ def taxi_trips_file(context: dg.AssetExecutionContext):
 
 
 @dg.asset(
-    deps=["taxi_trips_file"],
-    partitions_def=monthly_partition,
-    kinds={"duckdb"},
+  deps=["taxi_trips_file"],
+  partitions_def=monthly_partition,
 )
-def taxi_trips(context: dg.AssetExecutionContext, database: DuckDBResource):
-    """
+def taxi_trips(context: dg.AssetExecutionContext, database: DuckDBResource) -> None:
+  """
     The raw taxi trips dataset, loaded into a DuckDB database, partitioned by month.
-    """
+  """
 
-    partition_date_str = context.partition_key
-    month_to_fetch = partition_date_str[:-3]
+  partition_date_str = context.partition_key
+  month_to_fetch = partition_date_str[:-3]
 
-    query = f"""
-        create table if not exists trips (
-            vendor_id integer, pickup_zone_id integer, dropoff_zone_id integer,
-            rate_code_id double, payment_type integer, dropoff_datetime timestamp,
-            pickup_datetime timestamp, trip_distance double, passenger_count double,
-            total_amount double, partition_date varchar
-        );
+  query = f"""
+    create table if not exists trips (
+      vendor_id integer, pickup_zone_id integer, dropoff_zone_id integer,
+      rate_code_id double, payment_type integer, dropoff_datetime timestamp,
+      pickup_datetime timestamp, trip_distance double, passenger_count double,
+      total_amount double, partition_date varchar
+    );
 
-        delete from trips where partition_date = '{month_to_fetch}';
-    
-        insert into trips
-        select
-            VendorID, PULocationID, DOLocationID, RatecodeID, payment_type, tpep_dropoff_datetime, 
-            tpep_pickup_datetime, trip_distance, passenger_count, total_amount, '{month_to_fetch}' as partition_date
-        from '{constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)}';
-    """
+    delete from trips where partition_date = '{month_to_fetch}';
 
-    with database.get_connection() as conn:
-        conn.execute(query)
+    insert into trips
+    select
+      VendorID, PULocationID, DOLocationID, RatecodeID, payment_type, tpep_dropoff_datetime,
+      tpep_pickup_datetime, trip_distance, passenger_count, total_amount, '{month_to_fetch}' as partition_date
+    from '{constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)}';
+  """
+
+  with database.get_connection() as conn:
+      conn.execute(query)
--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -1,19 +1,19 @@
-# src/dagster_essentials/defs/jobs.py
 import dagster as dg
+from dagster_essentials.defs.partitions import monthly_partition, weekly_partition
 
-trips_by_week = dg.AssetSelection.assets(["trips_by_week"])
+# 1. Definição da seleção de assets
+trips_by_week = dg.AssetSelection.assets("trips_by_week")
 
+# 2. Job Mensal: Atualiza tudo, exceto o trips_by_week
 trip_update_job = dg.define_asset_job(
     name="trip_update_job",
+    partitions_def=monthly_partition,
     selection=dg.AssetSelection.all() - trips_by_week
 )
 
+# 3. Job Semanal: Atualiza especificamente o trips_by_week
 weekly_update_job = dg.define_asset_job(
     name="weekly_update_job",
+    partitions_def=weekly_partition,
     selection=trips_by_week,
 )
-
-
-@dg.job
-def jobs():
-    pass
